eca81c912638f3d86e2e04b6442779fab278d23f
DevVickyDharpure <dharpurevicky9@gmail.com>
2026-01-07 14:19:00 +0530

    Add files via upload

---
+++ practice-folder/JavaScript/class.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+let mark = prompt(" enter your mark")
+
+if (mark >= 90) {
+    console.log("A+")
+}
+else if (mark >= 85) {
+    console.log("A")
+}
+else if (mark >= 75) {+
+    console.log("B")
+}
+else if (mark >= 35) {
+    console.log("C")
+}
+else {
+    console.log("fail")
+}
+
+
+
+// 3 -----------------------------------------------------------
+// Write a program to check whether a character is a vowel or consonant.
+
+// let v = prompt("enter a vowel")
+
+// if (v == "a" || v == "e" ||v == "i" || v == "o" ||v == "u"){
+//     console.log("it's vowel")
+// }
+// else{
+//     console.log("it's consonant")
+// }
+// -----------------------------------------------------------
+
+
+
