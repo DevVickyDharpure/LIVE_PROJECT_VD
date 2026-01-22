40c7b13e57856db9900fa74ba65e687ed900735c
DevVickyDharpure <dharpurevicky9@gmail.com>
2026-01-22 15:38:23 +0530

    Add files via upload

---
+++ javaScript/Type-of-Function/prank.js
@@ -0,0 +1,13 @@
+console.log("Mobile Hack For Redmi || Oppo || Vivo")
+// start for hack 
+let i = prompt ("Enter your Mobile Number ")
+setTimeout(function(){
+alert("LOGGIN SUCCCESSFUL")
+alert("")
+alert("HAck () @ Mobile ")
+alert("HACK all (#@) Data ")
+alert("Hack")
+alert("Share your otp  in 20 min ")
+alert("BAN all Account ")
+     console.log("HaCK")
+},1000)
