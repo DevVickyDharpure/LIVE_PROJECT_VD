6bf5169688ecbd4c6834914b187d7deeba73ae7a
DevVickyDharpure <dharpurevicky9@gmail.com>
2026-01-28 19:54:56 +0530

    Add files via upload

---
+++ javaScript/JS_ARRY/class-1.js
@@ -0,0 +1,40 @@
+// console.log(" HELLO VICKY")  
+
+// // 
+// //CREATE ARRAY 
+// let numbers = [10, 20, 30, 40, 50];
+// numbers [2]=1
+
+// //index 0,1,2,3,4
+
+// let strings = ["string1", "string2", "string3"];
+// let answers = [true, false];
+
+// //ARRAY is collection of similar data type
+// let array = ["string", 10]
+
+// //ARRAY Head
+
+
+
+// console.log(numbers);
+// console.log(typeof (numbers[2]));
+
+// console.log(numbers[2], strings[1]);
+
+
+let arrry = [10 , 20 , 30 , 40 ,]
+
+
+
+console.log(arrry)
+
+
+arrry.unshift(10101)
+console.log("UN SHIFT")
+console.log(arrry)
+arrry.push(200)
+console.log("PUSH")
+console.log(arrry)
+arrry.pop(2)
+console.log(arrry)
